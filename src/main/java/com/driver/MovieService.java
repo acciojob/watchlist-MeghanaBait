4ef633e137ea22e8a4514f4f6dcc700c300ad78a
MeghanaBait <meghanawork777@gmail.com>
2023-08-19 20:02:36 +0530

    Adding Solution

--- src/main/java/com/driver/MovieService.java
+++ src/main/java/com/driver/MovieService.java
@@ -1,11 +1,13 @@
 package com.driver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MovieService {
+    @Autowired
     private MovieRepository movieRepository;
 
     public void addMovie(Movie movie) {
